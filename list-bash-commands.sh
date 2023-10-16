68a7748c92b75ea61c215caf66d0f6a40680a39e
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-10-16 21:21:09 +0300

    -

---
+++ list-bash-commands.sh
@@ -0,0 +1,43 @@
+#
+# lists bash commands in folder shell files using bcg container
+#
+# usage:
+#   bash list-bash-commands.sh folder reportfile
+#
+
+set -e
+
+container=BCG
+
+#
+# remove previous container
+#
+docker container rm ${container} &> /dev/null || true
+WD=${HD:-$(pwd)}   # host project directory
+
+if [ ! -d $1 ]
+then
+    echo "not a directory: $1"
+    exit 1
+fi
+
+#
+# mount and run
+#
+IMAGE=pasaopasen/bcg:latest
+docker pull $IMAGE
+docker run --name ${container}  \
+    -v "$WD"/$1:/srv/data \
+    $IMAGE \
+    python main.py data out.txt
+
+
+reportfile=${2:-bcg.txt}
+mkdir -p $(dirname $reportfile)
+
+docker cp ${container}:/srv/out.txt $reportfile
+
+cat $reportfile
+
+echo -e '\n\n\n---> Success'
+
